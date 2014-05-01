82fe8ff24065ac0e7bf0a604e185da08f2db77cf
Benjamin Hsieh <bhsiehh@gmail.com>
2014-05-01 01:36:07 -0700

    Redirect user after signing out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,9 @@ class ApplicationController < ActionController::Base
       return
     end
   end
+
+  # Overwriting the Devise sign_out redirect path method
+  def after_sign_out_path_for(resource_or_scope)
+    root_path
+  end
 end
