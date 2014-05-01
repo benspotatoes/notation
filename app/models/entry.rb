9cebc13e27b2fd1b910d1b07d80b42b3bac9ad3d
Benjamin Hsieh <bhsiehh@gmail.com>
2014-05-01 00:28:39 -0700

    Handle long titles by truncating text

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,17 +1,35 @@
 class Entry < ActiveRecord::Base
   belongs_to :user
 
-  TRUNCATED_LENGTH = 100
+  TRUNCATED_BODY_LENGTH = 100
+  TRUNCATED_LIST_TITLE_LENGTH = 10
+  TRUNCATED_DISP_TITLE_LENGTH = 25
 
   def markdown_preview
     RENDERER.render(body).html_safe
   end
 
   def truncated_body
-    body[0..TRUNCATED_LENGTH]
+    body[0..TRUNCATED_BODY_LENGTH]
   end
 
   def archive
     update_attribute(:archived, true)
+  end
+
+  def list_title
+    if title.length < TRUNCATED_LIST_TITLE_LENGTH
+      title
+    else
+      title[0..TRUNCATED_LIST_TITLE_LENGTH-3] + '...'
+    end
+  end
+
+  def disp_title
+    if title.length < TRUNCATED_DISP_TITLE_LENGTH
+      title
+    else
+      title[0..TRUNCATED_DISP_TITLE_LENGTH-3] + '...'
+    end
   end
 end
