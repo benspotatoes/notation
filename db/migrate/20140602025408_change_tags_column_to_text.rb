04b36ea0d1eb061c46cde2a87563b07a537b989b
Benjamin Hsieh <bhsiehh@gmail.com>
2014-06-01 20:04:08 -0700

    Reorder migration change

--- db/migrate/20140602025408_change_tags_column_to_text.rb
+++ db/migrate/20140602025408_change_tags_column_to_text.rb
@@ -1,6 +1,6 @@
 class ChangeTagsColumnToText < ActiveRecord::Migration
   def change
-    change_column :entries, :tags, :text
     change_column_default(:entries, :tags, nil)
+    change_column :entries, :tags, :text
   end
 end
