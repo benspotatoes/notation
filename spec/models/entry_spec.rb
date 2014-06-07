cb8753d82f2c282a71093aa808000e37f61c40d9
Benjamin Hsieh <bhsiehh@gmail.com>
2014-06-06 18:09:53 -0700

    Use a more meaningful test

--- spec/models/entry_spec.rb
+++ spec/models/entry_spec.rb
@@ -1,7 +1,11 @@
 require 'rails_helper'
 
 describe Entry do
-  it 'should exist' do
-    Entry
+  it 'can be created' do
+    entry = Entry.create!(
+      user_id: 1,
+      entry_type: :note)
+
+    expect(entry).to be_an_instance_of(Entry)
   end
 end
