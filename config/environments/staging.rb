5af9021c6a4e50327527944eb35797ad1c5aa116
Benjamin Hsieh <bhsiehh@gmail.com>
2014-06-07 19:25:40 -0700

    Add default host to staging env

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -80,4 +80,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Devise config
+  config.action_mailer.default_url_options = { host: 'notation.herokuapp.com' }
 end
